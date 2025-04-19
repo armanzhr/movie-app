83091024c48ddab372230dff9ec009173d8a2492
arman <armanzahraei@gmail.com>
2025-04-20 00:50:57 +0330

    add new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
         primary: "#030014",
-        scondary: "#151312",
+        secondary: "#151312",
         light: {
           100: "#D6C6FF",
           200: "#A8B5DB",
